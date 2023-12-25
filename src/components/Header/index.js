6f921d587f0207eae2f98636570fb979e38547ef
JSSINGH-1 <79414241+JSSINGH-1@users.noreply.github.com>
2023-12-25 20:28:04 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,27 @@
-// Write your JS code here
+import './index.css'
+
+const Header = () => (
+  <nav className="HeaderContainer">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+      alt="website logo"
+      className="image"
+    />
+    <ul className="itemContainer">
+      <li className="para">Home</li>
+      <li className="para">Products</li>
+      <li className="para">Cart</li>
+      <button className="buttonEl" type="button">
+        Logout
+      </button>
+    </ul>
+    <button className="navlogout" type="button">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+        alt="nav logout"
+        className="imageLogo"
+      />
+    </button>
+  </nav>
+)
+export default Header
